d309e4c4b304b19ddd1570699647ac470fdbf60d
Juan Dorado <fdorado985@gmail.com>
2020-09-20 13:55:05 -0500

    Remove unnecessary code from `ViewController`

--- Bullseye/ViewController.swift
+++ Bullseye/ViewController.swift
@@ -8,11 +8,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view.
-  }
-
   @IBAction func showAlert() {
     let alert = UIAlertController(title: "Hello World", message: "This is my first app!", preferredStyle: .alert)
     let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)

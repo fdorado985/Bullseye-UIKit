45ece873979531559d354bae0a14347472c78f97
Juan Dorado <fdorado985@gmail.com>
2020-09-20 13:58:42 -0500

    Create `sliderMoved` method and connect it with the `UISlider` on the `Storyboard`

--- Bullseye/ViewController.swift
+++ Bullseye/ViewController.swift
@@ -14,4 +14,8 @@ class ViewController: UIViewController {
     alert.addAction(action)
     present(alert, animated: true, completion: nil)
   }
+
+  @IBAction func sliderMoved(_ slider: UISlider) {
+    print("The value of the slider is now: \(slider.value)")
+  }
 }

f40a94cda5b7b170064253c4d99f4cef5aed20d8
Juan Dorado <fdorado985@gmail.com>
2020-09-20 14:20:20 -0500

    Add a `targetValue` property and initialize it with a `closeRange` between 1 and 100, and show it into the `alert`

--- Bullseye/ViewController.swift
+++ Bullseye/ViewController.swift
@@ -10,14 +10,16 @@ import UIKit
 class ViewController: UIViewController {
   @IBOutlet weak var slider: UISlider!
   var currentValue: Int = 0
+  var targetValue = 0
 
   override func viewDidLoad() {
     super.viewDidLoad()
     currentValue = lroundf(slider.value)
+    targetValue = Int.random(in: 1...100)
   }
 
   @IBAction func showAlert() {
-    let message = "The value of the slider is: \(currentValue)"
+    let message = "The value of the slider is: \(currentValue)" + "\nThe target value is \(targetValue)"
     let alert = UIAlertController(title: "Hello World", message: message, preferredStyle: .alert)
     let action = UIAlertAction(title: "OK", style: .default, handler: nil)
     alert.addAction(action)

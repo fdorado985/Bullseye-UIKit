bcaf7074ff1404787d8f5d90dd09297e6bd67947
Juan Dorado <fdorado985@gmail.com>
2020-09-20 23:33:32 -0500

    Style the About Screen

--- Bullseye/AboutViewController.swift
+++ Bullseye/AboutViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class AboutViewController: UIViewController {
-  override func viewDidLoad() {
-    super.viewDidLoad()
+  @IBAction func close() {
+    dismiss(animated: true, completion: nil)
   }
 }
